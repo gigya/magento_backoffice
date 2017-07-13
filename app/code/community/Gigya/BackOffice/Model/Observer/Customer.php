6556afb0231c918e7b94f45981400b025dd5eba5
Cristi <cristian.vatca@osf-global.com>
2017-07-13 11:55:16 +0300

    Fix incompatibility reported by magento marketplace.

--- app/code/community/Gigya/BackOffice/Model/Observer/Customer.php
+++ app/code/community/Gigya/BackOffice/Model/Observer/Customer.php
@@ -6,33 +6,28 @@
  * Date: 7/26/16
  * Time: 10:31 AM
  */
-class Gigya_BackOffice_Model_Observer_Customer {
-
-  public function refreshFromGigya($observer) {
-    $exec = Mage::registry("gigya_backoffice_refresh_executed");
-    if (isset($_SESSION['adminhtml']) && null === $exec && count($_POST) === 0) {
-      /** @var Mage_Customer_Model_Customer $customer */
-      $customer  = $observer->getData('customer');
-      $gigya_uid = $customer->getData('gigya_uid');
-      /** @var Gigya_Social_Helper_Data $helper */
-      $helper       = Mage::helper('Gigya_Social');
-      $gigyaAccount = $helper->utils->getAccount($gigya_uid);
-      if (is_numeric($gigyaAccount)) {
-        Mage:
-        log(
-          "Error retrieving account from gigya error code was " . $gigyaAccount
-        );
-      }
-      else {
-        $updater = new Gigya_Social_Helper_FieldMapping_MagentoUpdater(
-          $gigyaAccount
-        );
-        if ($updater->isMapped()) {
-          $updater->updateMagentoAccount($customer);
+class Gigya_BackOffice_Model_Observer_Customer
+{
+    public function refreshFromGigya($observer) {
+        $exec = Mage::registry("gigya_backoffice_refresh_executed");
+        $adminSession = Mage::getSingleton('admin/session', array('name' => 'adminhtml'))->isLoggedIn();
+        if ($adminSession === true && null === $exec && count(Mage::app()->getRequest()->getPost()) === 0) {
+            /** @var Mage_Customer_Model_Customer $customer */
+            $customer = $observer->getData('customer');
+            $gigya_uid = $customer->getData('gigya_uid');
+            /** @var Gigya_Social_Helper_Data $helper */
+            $helper = Mage::helper('Gigya_Social');
+            $gigyaAccount = $helper->utils->getAccount($gigya_uid);
+            if (is_numeric($gigyaAccount)) {
+                Mage::log("Error retrieving account from gigya error code was " . $gigyaAccount);
+            } else {
+                $updater = new Gigya_Social_Helper_FieldMapping_MagentoUpdater($gigyaAccount);
+                if ($updater->isMapped()) {
+                    $updater->updateMagentoAccount($customer);
+                }
+            }
+            Mage::register("gigya_backoffice_refresh_executed", TRUE);
         }
-      }
-      Mage::register("gigya_backoffice_refresh_executed", TRUE);
-    }
-  }
 
+    }
 }
